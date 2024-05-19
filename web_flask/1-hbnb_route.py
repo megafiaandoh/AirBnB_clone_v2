cf1797aaa971d7449a61cde81dc0dafcc5cf41f7
megafiaandoh <andoh26@gmail.com>
2024-05-19 08:04:30 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-""" 1. Script to start a Flask web application with 2 commands """
-
+"""Simple Flask web application"""
 from flask import Flask
-
-
-app = Flask(__name__)
+app = Flask('web_flask')
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    """ Returns some text. """
+def hello_route1():
+    """Return 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello():
-    """ Return other text. """
+def hello_route2():
+    """Return 'HBNB'"""
     return 'HBNB'
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
