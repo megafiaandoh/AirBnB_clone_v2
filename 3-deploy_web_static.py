ceb1ccf3f347eb23b52eb3f16813bcdec1aef0a8
megafiaandoh <andoh26@gmail.com>
2024-05-19 07:51:01 -0700

    0-hello_route.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["54.87.169.212", "54.89.21.157"]
 
 
 def do_pack():
